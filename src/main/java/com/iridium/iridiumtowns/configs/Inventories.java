cde055bb729be97ce80a31de722925c5bc43b3c8
Peaches_MLG <PeachesMLG100@gmail.com>
2022-08-06 15:39:35 +0100

    Added Back Buttons + Main Command GUI

--- src/main/java/com/iridium/iridiumtowns/configs/Inventories.java
+++ src/main/java/com/iridium/iridiumtowns/configs/Inventories.java
@@ -1,6 +1,32 @@
 package com.iridium.iridiumtowns.configs;
 
+import com.google.common.collect.ImmutableMap;
+import com.iridium.iridiumcore.Background;
+import com.iridium.iridiumcore.Item;
+import com.iridium.iridiumcore.dependencies.fasterxml.annotation.JsonIgnore;
+import com.iridium.iridiumcore.dependencies.xseries.XMaterial;
+import com.iridium.iridiumteams.configs.inventories.InventoryConfig;
+
+import java.util.Collections;
+import java.util.HashMap;
+
 public class Inventories extends com.iridium.iridiumteams.configs.Inventories {
+    @JsonIgnore
+    private final Background background1 = new Background(new HashMap<>());
+
+    public InventoryConfig townMenu = new InventoryConfig(45, "&7Town Menu", background1, ImmutableMap.<String, Item>builder()
+            .put("t home", new Item(XMaterial.WHITE_BED, 13, 1, "&b&lTown Home", Collections.singletonList("&7Teleport to your town home")))
+            .put("t boosters", new Item(XMaterial.EXPERIENCE_BOTTLE, 21, 1, "&b&lTown Boosters", Collections.singletonList("&7View your town boosters")))
+            .put("t members", new Item(XMaterial.PLAYER_HEAD, 22, 1, "&b&lTown Members", "Peaches_MLG", Collections.singletonList("&7View your town members")))
+            .put("t warps", new Item(XMaterial.END_PORTAL_FRAME, 23, 1, "&b&lTown Warps", Collections.singletonList("&7View your town warps")))
+            .put("t upgrade", new Item(XMaterial.DIAMOND, 29, 1, "&b&lTown Upgrades", Collections.singletonList("&7View your town upgrades")))
+            .put("t missions", new Item(XMaterial.IRON_SWORD, 30, 1, "&b&lTown Missions", Collections.singletonList("&7View your town missions")))
+            .put("t bank", new Item(XMaterial.PLAYER_HEAD, 31, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvODM4MWM1MjlkNTJlMDNjZDc0YzNiZjM4YmI2YmEzZmRlMTMzN2FlOWJmNTAzMzJmYWE4ODllMGEyOGU4MDgxZiJ9fX0", 1, "&b&lTown Bank", Collections.singletonList("&7View your town bank")))
+            .put("t permissions", new Item(XMaterial.WRITABLE_BOOK, 32, 1, "&b&lTown Permissions", Collections.singletonList("&7View your town permissions")))
+            .put("t delete", new Item(XMaterial.BARRIER, 33, 1, "&b&lDelete Town", Collections.singletonList("&7Delete your town")))
+            .build()
+    );
+
     public Inventories() {
         super("Town", "&2");
     }
--- src/main/java/com/iridium/iridiumtowns/managers/CommandManager.java
+++ src/main/java/com/iridium/iridiumtowns/managers/CommandManager.java
@@ -1,10 +1,12 @@
 package com.iridium.iridiumtowns.managers;
 
+import com.iridium.iridiumteams.gui.InventoryConfigGUI;
 import com.iridium.iridiumtowns.IridiumTowns;
 import com.iridium.iridiumtowns.database.Town;
 import com.iridium.iridiumtowns.database.User;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class CommandManager extends com.iridium.iridiumteams.managers.CommandManager<Town, User> {
@@ -15,6 +17,14 @@ public class CommandManager extends com.iridium.iridiumteams.managers.CommandMan
 
     @Override
     public void noArgsDefault(@NotNull CommandSender commandSender) {
-        Bukkit.getServer().dispatchCommand(commandSender, "town help");
+        if (commandSender instanceof Player) {
+            Player player = (Player) commandSender;
+            User user = IridiumTowns.getInstance().getUserManager().getUser(player);
+            if (IridiumTowns.getInstance().getTeamManager().getTeamViaID(user.getTeamID()).isPresent()) {
+                player.openInventory(new InventoryConfigGUI(IridiumTowns.getInstance().getInventories().townMenu).getInventory());
+                return;
+            }
+            Bukkit.getServer().dispatchCommand(commandSender, "town help");
+        }
     }
 }
